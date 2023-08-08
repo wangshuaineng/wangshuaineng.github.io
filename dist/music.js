5b12f16af3e9d2a8f24547381119299073aad2ae
王帅能 <15037392412@163.com>
2023-08-08 22:00:44 +0800

    Site updated: 2023-08-08 22:00:43

---
+++ dist/music.js
@@ -0,0 +1,64 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+      {
+        name: '家走',
+        artist: 'Newyounggo / 艾热 AIR',
+        url: 'http://music.163.com/song/media/outer/url?id=538225861.mp3',
+        cover: 'http://p1.music.126.net/J1wRWZmAGer18ChuGxnNEw==/109951163146492998.jpg?param=130y130',
+      },
+      {
+        name: 'All Girls Are The Same',
+        artist: 'Juice WRLD',
+        url: 'http://music.163.com/song/media/outer/url?id=548823873.mp3',
+        cover: 'http://p2.music.126.net/di5piIGtE69x_8dv4NuV1g==/109951163244465319.jpg?param=130y130',
+      },
+      {
+        name: 'My Back',
+        artist: 'mac ova seas',
+        url: 'http://music.163.com/song/media/outer/url?id=1411157152.mp3',
+        cover: 'http://p1.music.126.net/z5hEwkgBs1bbSikNHaircw==/109951164571698372.jpg?param=130y130',
+      },
+      {
+        name: '很高兴认识你',
+        artist: 'C-BLOCK',
+        url: 'http://music.163.com/song/media/outer/url?id=1318571230.mp3',
+        cover: 'http://p2.music.126.net/UU09arK_fRlyWVdh2znikA==/109951163610766464.jpg?param=130y130',
+      },
+      {
+        name: '伤心男孩拯救世界',
+        artist: '河南说唱之神 / 荨麻疹',
+        url: 'http://music.163.com/song/media/outer/url?id=1379728692.mp3',
+        cover: 'http://p2.music.126.net/7-6A_PuEWERADEHcN-5h3A==/109951164234750541.jpg?param=130y130',
+      },
+      {
+        name: '大气层',
+        artist: '法老 / 郑宇界',
+        url: 'http://music.163.com/song/media/outer/url?id=1334297124.mp3',
+        cover: 'http://p1.music.126.net/wTO7xsKa9YbeZGtgMzDZGw==/109951163807651102.jpg?param=130y130',
+      },
+      {
+        name: 'Wild Star',
+        artist: 'KIV',
+        url: 'http://music.163.com/song/media/outer/url?id=1311663691.mp3',
+        cover: 'http://p1.music.126.net/9CWpI9uLWQSIY1aWGY8FcQ==/109951163561295305.jpg?param=130y130',
+      },
+      {
+        name: '恼火 remix',
+        artist: 'KenRobb / Uzzy',
+        url: 'http://music.163.com/song/media/outer/url?id=1955501364.mp3',
+        cover: 'http://p1.music.126.net/Etvl7SX_2b3elxdAf6xwBA==/109951167544834964.jpg?param=130y130',
+      },
+      {
+        name: 'WECHAT',
+        artist: '狠毒男孩 / 嫩桃弟弟',
+        url: 'http://music.163.com/song/media/outer/url?id=1824132059.mp3',
+        cover: 'http://p2.music.126.net/UkkWPGkJD9zKwY1khp_fjA==/109951165771611226.jpg?param=130y130',
+      },
+      
+    ]
+  });
+  
+  
